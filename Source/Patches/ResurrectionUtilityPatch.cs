20683ab2ac0ef208e4d913fb313bab0e44474210
SirRandoo <randomshovel@gmail.com>
2020-06-10 08:42:38 -0500

    Removed old commented patch.

--- Source/Patches/ResurrectionUtilityPatch.cs
+++ Source/Patches/ResurrectionUtilityPatch.cs
@@ -7,31 +7,6 @@ namespace SirRandoo.RDA.Patches
     [HarmonyPatch(typeof(ResurrectionUtility), "Resurrect")]
     public static class ResurrectionUtility__Resurrect
     {
-        // [HarmonyTranspiler]
-        // public static IEnumerable<CodeInstruction> Resurrect(IEnumerable<CodeInstruction> instructions)
-        // {
-        //     var enableAndInit = AccessTools.Method(
-        //         typeof(Pawn_WorkSettings),
-        //         nameof(Pawn_WorkSettings.EnableAndInitialize)
-        //     );
-        //     var enableAndInitIf = AccessTools.Method(
-        //         typeof(Pawn_WorkSettings),
-        //         nameof(Pawn_WorkSettings.EnableAndInitializeIfNotAlreadyInitialized)
-        //     );
-        //
-        //     foreach (var instruction in instructions)
-        //     {
-        //         if (instruction.opcode == OpCodes.Callvirt && instruction.operand as MethodInfo == enableAndInit)
-        //         {
-        //             yield return new CodeInstruction(OpCodes.Callvirt, enableAndInitIf);
-        //         }
-        //         else
-        //         {
-        //             yield return instruction;
-        //         }
-        //     }
-        // }
-
         [HarmonyPostfix]
         public static void ResurrectPostfix(Pawn pawn)
         {
